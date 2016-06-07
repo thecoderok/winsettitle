6a05bfed0aeb66520cc010558abd7e11cb0dd0c6
Vitalii Ganzha <vitaliiy@outlook.com>
2016-06-07 11:45:04 -0700

    prevent application closing

--- WinSetApplicationTitle/MainForm.Designer.cs
+++ WinSetApplicationTitle/MainForm.Designer.cs
@@ -31,15 +31,20 @@ namespace WinSetApplicationTitle
             this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
             this.trayIcon = new System.Windows.Forms.NotifyIcon(this.components);
-            this.mainTabControl = new System.Windows.Forms.TabControl();
-            this.tabPage1 = new System.Windows.Forms.TabPage();
-            this.tabPage2 = new System.Windows.Forms.TabPage();
             this.trayContextMenuStrip = new System.Windows.Forms.ContextMenuStrip(this.components);
             this.showApplicationToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
             this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.mainTabControl.SuspendLayout();
+            this.mainTabControl = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.chkHideWindowOnStartup = new System.Windows.Forms.CheckBox();
+            this.chkStartAppAutomatically = new System.Windows.Forms.CheckBox();
+            this.btnExit = new System.Windows.Forms.Button();
             this.trayContextMenuStrip.SuspendLayout();
+            this.mainTabControl.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
             this.SuspendLayout();
             // 
             // trayIcon
@@ -48,37 +53,6 @@ namespace WinSetApplicationTitle
             this.trayIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("trayIcon.Icon")));
             this.trayIcon.Text = "WinSetApplicationTitle";
             this.trayIcon.Visible = true;
-            // 
-            // mainTabControl
-            // 
-            this.mainTabControl.Controls.Add(this.tabPage1);
-            this.mainTabControl.Controls.Add(this.tabPage2);
-            this.mainTabControl.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.mainTabControl.Location = new System.Drawing.Point(0, 0);
-            this.mainTabControl.Name = "mainTabControl";
-            this.mainTabControl.SelectedIndex = 0;
-            this.mainTabControl.Size = new System.Drawing.Size(284, 261);
-            this.mainTabControl.TabIndex = 0;
-            // 
-            // tabPage1
-            // 
-            this.tabPage1.Location = new System.Drawing.Point(4, 22);
-            this.tabPage1.Name = "tabPage1";
-            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(276, 235);
-            this.tabPage1.TabIndex = 0;
-            this.tabPage1.Text = "tabPage1";
-            this.tabPage1.UseVisualStyleBackColor = true;
-            // 
-            // tabPage2
-            // 
-            this.tabPage2.Location = new System.Drawing.Point(4, 22);
-            this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(276, 235);
-            this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "Settings";
-            this.tabPage2.UseVisualStyleBackColor = true;
             // 
             // trayContextMenuStrip
             // 
@@ -94,6 +68,7 @@ namespace WinSetApplicationTitle
             this.showApplicationToolStripMenuItem.Name = "showApplicationToolStripMenuItem";
             this.showApplicationToolStripMenuItem.Size = new System.Drawing.Size(165, 22);
             this.showApplicationToolStripMenuItem.Text = "Show application";
+            this.showApplicationToolStripMenuItem.Click += new System.EventHandler(this.showApplicationToolStripMenuItem_Click);
             // 
             // toolStripMenuItem1
             // 
@@ -106,6 +81,70 @@ namespace WinSetApplicationTitle
             this.exitToolStripMenuItem.Size = new System.Drawing.Size(165, 22);
             this.exitToolStripMenuItem.Text = "Exit";
             // 
+            // mainTabControl
+            // 
+            this.mainTabControl.Controls.Add(this.tabPage1);
+            this.mainTabControl.Controls.Add(this.tabPage2);
+            this.mainTabControl.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.mainTabControl.Location = new System.Drawing.Point(0, 0);
+            this.mainTabControl.Name = "mainTabControl";
+            this.mainTabControl.SelectedIndex = 0;
+            this.mainTabControl.Size = new System.Drawing.Size(284, 261);
+            this.mainTabControl.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.btnExit);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(276, 235);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Main";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.chkHideWindowOnStartup);
+            this.tabPage2.Controls.Add(this.chkStartAppAutomatically);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(276, 235);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Settings";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // chkHideWindowOnStartup
+            // 
+            this.chkHideWindowOnStartup.AutoSize = true;
+            this.chkHideWindowOnStartup.Location = new System.Drawing.Point(8, 29);
+            this.chkHideWindowOnStartup.Name = "chkHideWindowOnStartup";
+            this.chkHideWindowOnStartup.Size = new System.Drawing.Size(137, 17);
+            this.chkHideWindowOnStartup.TabIndex = 3;
+            this.chkHideWindowOnStartup.Text = "Hide window on startup";
+            this.chkHideWindowOnStartup.UseVisualStyleBackColor = true;
+            // 
+            // chkStartAppAutomatically
+            // 
+            this.chkStartAppAutomatically.AutoSize = true;
+            this.chkStartAppAutomatically.Location = new System.Drawing.Point(8, 6);
+            this.chkStartAppAutomatically.Name = "chkStartAppAutomatically";
+            this.chkStartAppAutomatically.Size = new System.Drawing.Size(166, 17);
+            this.chkStartAppAutomatically.TabIndex = 2;
+            this.chkStartAppAutomatically.Text = "Start application automatically";
+            this.chkStartAppAutomatically.UseVisualStyleBackColor = true;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(8, 6);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 0;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -115,8 +154,12 @@ namespace WinSetApplicationTitle
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "MainForm";
             this.Text = "WinSetApplicationTitle";
-            this.mainTabControl.ResumeLayout(false);
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
             this.trayContextMenuStrip.ResumeLayout(false);
+            this.mainTabControl.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -131,6 +174,9 @@ namespace WinSetApplicationTitle
         private System.Windows.Forms.ToolStripMenuItem showApplicationToolStripMenuItem;
         private System.Windows.Forms.ToolStripSeparator toolStripMenuItem1;
         private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.CheckBox chkHideWindowOnStartup;
+        private System.Windows.Forms.CheckBox chkStartAppAutomatically;
+        private System.Windows.Forms.Button btnExit;
     }
 }
 
--- WinSetApplicationTitle/MainForm.cs
+++ WinSetApplicationTitle/MainForm.cs
@@ -1,16 +1,23 @@
-﻿using System;
+using System;
 using System.Windows.Forms;
 
 namespace WinSetApplicationTitle
 {
     public partial class MainForm : Form
     {
+        private static bool canCloseForm = false;
+
         public MainForm()
         {
             InitializeComponent();
             var helper = new HotKeysHelper();
             helper.RegisterHotKey(WinSetApplicationTitle.ModifierKeys.Control | WinSetApplicationTitle.ModifierKeys.Shift, Keys.Z);
             helper.KeyPressed += Helper_KeyPressed;
+        }
+
+        public static void AllowFormClosing()
+        {
+            canCloseForm = true;
         }
 
         private void Helper_KeyPressed(object sender, KeyPressedEventArgs e)
@@ -27,6 +34,29 @@ namespace WinSetApplicationTitle
             Array.Reverse(arr);
 
             titleSetter.SetTitleByMainWindowHandle(window.MainWindowHandle, new string(arr));
+        }
+
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (e.CloseReason != CloseReason.UserClosing || canCloseForm)
+            {
+                return;
+            }
+
+            e.Cancel = true;
+            this.Hide();
+            this.trayIcon.Visible = true;
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            AllowFormClosing();
+            this.Close();
+        }
+
+        private void showApplicationToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Show();
         }
     }
 }
